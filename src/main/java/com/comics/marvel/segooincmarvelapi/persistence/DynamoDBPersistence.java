a6ed5b2ff13295950da87bedcd9a1f3d62c7ca74
Letlhogonolo Segoe <noloxtreme@gmail.com>
2019-08-18 23:06:37 +0200

    Saving comic book data to Dynamo DB

--- src/main/java/com/comics/marvel/segooincmarvelapi/persistence/DynamoDBPersistence.java
+++ src/main/java/com/comics/marvel/segooincmarvelapi/persistence/DynamoDBPersistence.java
@@ -1,13 +1,15 @@
 package com.comics.marvel.segooincmarvelapi.persistence;
 
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.Table;
+import com.google.gson.GsonBuilder;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Component
 public class DynamoDBPersistence implements Persistence{
@@ -22,7 +24,20 @@ public class DynamoDBPersistence implements Persistence{
             Table table = dynamoDB.getTable("marvel-comic-books");
 
             List<Map> results = (List<Map>) ((Map) comics.get("data")).get("results");
-            System.out.println(results.get(0));
+
+            if (results!=null) {
+                for (Map result:results) {
+                    result = removeNullValues(result);
+                    Item item = new Item();
+
+                    for (Object key:result.keySet()) {
+                        item.with((String) key,result.get(key));
+                   }
+
+                    table.putItem(item);
+                    System.out.printf("Successfully saved %s\n",result.get("title"));
+                }
+            }
         }
         catch(Exception e) {
             throw new RuntimeException(e);
@@ -30,6 +45,21 @@ public class DynamoDBPersistence implements Persistence{
     }
 
     private Map removeNullValues(Map map) {
+        List<String> list = (List<String>) map.keySet().stream().map(key->key.toString()).collect(Collectors.toList());
+        for (String key:list) {
+            Object item = map.get(key);
+            if (item==null) {
+                map.remove(key);
+                continue;
+            }
+            if (item instanceof String && StringUtils.isEmpty((String)item)) {
+                map.remove(key);
+                continue;
+            }
+            if (item instanceof Map) {
+                removeNullValues((Map) item);
+            }
+        }
 
         return map;
     }
